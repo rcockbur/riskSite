6c63fad0a1512dc3db9aad5fd0ec7e5e1ef314d8
ross <rcockbur@ualberta.ca>
2017-08-13 20:59:08 -0700

    stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root :to => 'application#home'
+  root :to => 'application#home', :as => '/home'
+  get '/home', :to => 'application#home'
   get '/units', :to => 'application#units'
 end
